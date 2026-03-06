a9082b5a381859fb79fe2d659106ebb2a781a68c
ifarx90 <imrafi9205@gmail.com>
2026-03-07 02:38:51 +0700

    add express server

--- index.js
+++ index.js
@@ -25,3 +25,17 @@ async function startBot() {
 }
 
 startBot();
+
+const express = require("express")
+
+const app = express()
+
+app.get("/", (req, res) => {
+  res.send("Bot is running")
+})
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log("Web server running on port", PORT)
+})
