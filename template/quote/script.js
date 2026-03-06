770158507a25022abab0f3357199c929bb7f8f6d
ifarx90 <imrafi9205@gmail.com>
2026-03-07 04:00:41 +0700

    update ui wa

--- template/quote/script.js
+++ template/quote/script.js
@@ -1,22 +1,37 @@
-const params = new URLSearchParams(window.location.search)
+const params = new URLSearchParams(window.location.search);
 
-const text = params.get("text")
+const text = params.get("text");
 
-const message = document.getElementById("message")
+const message = document.getElementById("message");
 
-if(text){
-message.innerText = text
-}else{
-message.innerText = "(teks isi sendiri)"
+// ========================
+// RENDER TEXT + EMOJI
+// ========================
+
+function renderEmoji(text) {
+  return twemoji.parse(text, {
+    folder: "svg",
+    ext: ".svg",
+  });
 }
 
-const now = new Date()
-
-let h = now.getHours()
-let m = now.getMinutes()
-
-if(m < 10){
-m = "0" + m
+if (text) {
+  message.innerHTML = renderEmoji(text);
+} else {
+  message.innerText = "(teks isi sendiri)";
 }
 
-document.getElementById("time").innerText = h + ":" + m
+// ========================
+// TIME
+// ========================
+
+const now = new Date();
+
+let h = now.getHours();
+let m = now.getMinutes();
+
+if (m < 10) {
+  m = "0" + m;
+}
+
+document.getElementById("time").innerText = h + ":" + m;
